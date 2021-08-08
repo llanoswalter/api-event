ff7e3a1518fb167de57760d414375b4f1c1d55a6
Llanos Walter <llanoswaltere@gmail.com>
2021-08-08 15:58:18 -0300

    production master V2.0.0

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const eventRoutes = require("./event/eventRoutes");
+const userRoutes = require("./user/userRoutes");
+const loginRoutes = require("./login/loginRoutes");
 
 module.exports = function () {
   router.get("/", (req, res) => {
     res.send("Hello Work");
   });
+  router.use("/api/users/", userRoutes());
 
   return router;
 };
